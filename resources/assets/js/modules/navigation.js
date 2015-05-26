aacf1f86852ad4f727a96af543c0bc67f95e60a1
Joe Cianflone <joe@cianflone.co>
2015-05-25 22:59:24 -0400

    nav works better

--- resources/assets/js/modules/navigation.js
+++ resources/assets/js/modules/navigation.js
@@ -3,6 +3,7 @@ App.Modules = App.Modules || {};
 App.Modules.Navigation = function () {
    var options = {
       wrapper:   ".js-wrapper",
+      hide:      ".js-toggle-hide",
       toggler:   ".js-menu-toggler",
       container: ".js-secondary-content"
    };
@@ -10,19 +11,23 @@ App.Modules.Navigation = function () {
    var toggleNav = function(data) {
       var container = data.eventElement.closest(options.container);
 
-      container.find(options.wrapper).toggle();
+      container.find(options.hide).toggle();
 
       if (container.hasClass("js-container-closed")) {
          container.attr("style", "")
-                  .find(options.toggler).attr("style", "")
-                  .removeClass("js-container-closed");
+                  .removeClass("js-container-closed")
+                  .find(options.wrapper).attr("style", "")
+                  .find(options.toggler).attr("style", "");
 
          return false;
       }
 
       container.addClass("js-container-closed")
-               .find(options.toggler).css("padding", "18px 16px 18px 0")
-               .css({"flex": "0 0 50px"});
+               .css({"flex": "0 0 50px"})
+               .find(options.wrapper).css({
+                  "width": "38px",
+                  "padding": "0"
+               });
 
       return false;
    };
--- resources/views/fragments/layout/secondary.blade.php
+++ resources/views/fragments/layout/secondary.blade.php
@@ -1,43 +1,45 @@
 <section class="secondary-content js-secondary-content">
-   <a href="#" class="menu-toggler js-menu-toggler" title="Toggle Sidebar">
-      <span class="menu-toggler__bars fa fa-bars"></span>
-      <span class="menu-toggler__text">nav</span>
-   </a>
    <div class="wrapper js-wrapper">
-      <nav>
-         <ul class="navigation-list">
-            <li class="navigation-list__item is-selected"><a href="#">home</a>
-               <ul class="navigation-list--secondary">
-                  <li class="navigation-list__item"><a href="#">#stream</a></li>
-                  <li class="navigation-list__item"><a href="#">#tweets</a></li>
-                  <li class="navigation-list__item"><a href="#">#github</a></li>
-                  <li class="navigation-list__item"><a href="#">#videos</a></li>
-                  <li class="navigation-list__item"><a href="#">#articles</a></li>
-               </ul>
-            </li>
-            <li class="navigation-list__item"><a href="#">about</a></li>
-            <li class="navigation-list__item"><a href="#">feed</a></li>
-         </ul>
-         <hr>
-         <ul class="navigation-list">
-            <li class="navigation-list__item"><a href="#">Projects &amp; Contributions</a>
-               <ul class="navigation-list--secondary">
-                  <li class="navigation-list__item"><a href="#">heisenberg</a></li>
-                  <li class="navigation-list__item"><a href="#">laravel-elixir-sass-compass</a></li>
-               </ul>
-            </li>
-         </ul>
-         <hr>
-         <ul class="navigation-list--icons">
-            <li class="navigation-list__item"><a href="//twitter.com/JoeCianflone" target="_blank" title="Twitter"><span class="fa fa-twitter"></span></a></li>
-            <li class="navigation-list__item"><a href="//github.com/JoeCianflone" target="_blank" title="Github"><span class="fa fa-github"></span></a></li>
-            <li class="navigation-list__item"><a href="//youtube.com/JoeCianflone" target="_blank" title="YouTube"><span class="fa fa-youtube"></span></a></li>
-            <li class="navigation-list__item"><a href="#" title="Contact"><span class="fa fa-envelope"></span></a></li>
-         </ul>
-      </nav>
-      <footer>
-         <p>Everything in this site is my own opinion and does not express the views of any company I may have or currently do work for and with.</p>
-         <p>This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.</p>
-      </footer>
+      <a href="#" class="menu-toggler js-menu-toggler" title="Toggle Sidebar">
+         <span class="menu-toggler__bars fa fa-bars"></span>
+         <span class="menu-toggler__text">nav</span>
+      </a>
+      <div class="js-toggle-hide">
+         <nav>
+            <ul class="navigation-list">
+               <li class="navigation-list__item is-selected"><a href="#">home</a>
+                  <ul class="navigation-list--secondary">
+                     <li class="navigation-list__item"><a href="#">#stream</a></li>
+                     <li class="navigation-list__item"><a href="#">#tweets</a></li>
+                     <li class="navigation-list__item"><a href="#">#github</a></li>
+                     <li class="navigation-list__item"><a href="#">#videos</a></li>
+                     <li class="navigation-list__item"><a href="#">#articles</a></li>
+                  </ul>
+               </li>
+               <li class="navigation-list__item"><a href="#">about</a></li>
+               <li class="navigation-list__item"><a href="#">feed</a></li>
+            </ul>
+            <hr>
+            <ul class="navigation-list">
+               <li class="navigation-list__item"><a href="#">Projects &amp; Contributions</a>
+                  <ul class="navigation-list--secondary">
+                     <li class="navigation-list__item"><a href="#">heisenberg</a></li>
+                     <li class="navigation-list__item"><a href="#">laravel-elixir-sass-compass</a></li>
+                  </ul>
+               </li>
+            </ul>
+            <hr>
+            <ul class="navigation-list--icons">
+               <li class="navigation-list__item"><a href="//twitter.com/JoeCianflone" target="_blank" title="Twitter"><span class="fa fa-twitter"></span></a></li>
+               <li class="navigation-list__item"><a href="//github.com/JoeCianflone" target="_blank" title="Github"><span class="fa fa-github"></span></a></li>
+               <li class="navigation-list__item"><a href="//youtube.com/JoeCianflone" target="_blank" title="YouTube"><span class="fa fa-youtube"></span></a></li>
+               <li class="navigation-list__item"><a href="#" title="Contact"><span class="fa fa-envelope"></span></a></li>
+            </ul>
+         </nav>
+         <footer>
+            <p>Everything in this site is my own opinion and does not express the views of any company I may have or currently do work for and with.</p>
+            <p>This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.</p>
+         </footer>
+      </div>
    </div>
 </section>
