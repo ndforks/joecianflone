8cd63417e2753fd723b351228a77a52ab440810e
Joe Cianflone <joe@cianflone.co>
2016-01-05 23:41:53 -0500

    testing cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
    */
    protected function schedule(Schedule $schedule)
    {
-      $schedule->command('stream:tweets')->hourly();
+      $schedule->command('stream:tweets')->everyMinute();
       $schedule->command('stream:articles')->dailyAt('9:30');
    }
 }
