0fcfc5cd17adda6d47c0fc3160a249433b70f6f2
Joe Cianflone <joe@cianflone.co>
2016-07-17 18:32:24 -0400

    updates to retweets and adds links to all tweets

--- resources/views/fragments/stream/tweet.blade.php
+++ resources/views/fragments/stream/tweet.blade.php
@@ -12,29 +12,28 @@
          @endif
          <div class={!! isset($element->content->entities->media) ? "mediabox__text" : "mediabox__text--full" !!}>
              @if ($element->content->retweeted)
-                 <p class="tweet-block__text">{!! Twitter::linkify($element->content->retweeted_status->text) !!}</p>
+                 <p class="tweet-block__text">
+                     {!! Twitter::linkify($element->content->retweeted_status->text) !!}
+                 </p>
              @else
                  <p class="tweet-block__text">{!! Twitter::linkify($element->content->text) !!}</p>
              @endif
          </div>
          @if (isset($element->content->entities->media))
             <div class="meta-data--inside">
+         @else
+             <div class="meta-data--full">
+         @endif
                <ul class="meta-list">
-                  <li class="meta-list__item"><span class="fa fa-twitter"></span></li>
-                  <li class="meta-list__item">{{$element->content->retweeted_status->user->screen_name or 'JoeCianflone' }}</li>
+                  @if ($element->content->retweeted)
+                      <li class="meta-list__item"><span class="fa fa-retweet" aria-hidden="true"></span></li>
+                  @else
+                      <li class="meta-list__item"><span class="fa fa-twitter"></span></li>
+                  @endif
+                  <li class="meta-list__item"><a href="//twitter.com/statuses/{{$element->item_id}}" title="">&#64;{{$element->content->retweeted_status->user->screen_name or 'JoeCianflone' }}</a></li>
                   <li class="meta-list__item"><time data-livestamp="{{ $element->item_created_at }}" datetime="{{ $element->item_created_at }}">{{ $element->item_created_at }}</time></li>
                </ul>
             </div>
-         @endif
       </div>
-      @if (! isset($element->content->entities->media))
-         <div class="meta-data--full">
-            <ul class="meta-list">
-               <li class="meta-list__item"><span class="fa fa-twitter"></span></li>
-               <li class="meta-list__item">{{$element->content->retweeted_status->user->screen_name or 'JoeCianflone' }}</li>
-               <li class="meta-list__item"><time data-livestamp="{{ $element->item_created_at }}" datetime="{{ $element->item_created_at }}">{{ $element->item_created_at }}</time></li>
-            </ul>
-         </div>
-      @endif
    </blockquote>
 </article>
