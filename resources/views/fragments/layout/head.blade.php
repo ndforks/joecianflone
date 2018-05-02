117a375f71a8d18dfc679d0ff16dcf03a7bf571f
Joe Cianflone <joe@cianflone.co>
2018-05-02 10:28:29 -0400

    move analytics to head

--- resources/views/fragments/layout/head.blade.php
+++ resources/views/fragments/layout/head.blade.php
@@ -27,3 +27,13 @@
 
 <!-- loading up our css -->
 <link rel="stylesheet" type="text/css" href="/assets/css/application.css">
+
+@if (App::environment('production'))
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118593693-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-118593693-1');
+    </script>
+@endif
--- resources/views/fragments/layout/scripts.blade.php
+++ resources/views/fragments/layout/scripts.blade.php
@@ -1,13 +1,3 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="{{ asset("/assets/js/jquery.min.js") }}"><\/script>')</script>
 <script src="{{ asset('/assets/js/app.min.js') }}"></script>
-
-@if (App::environment('production'))
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118593693-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-118593693-1');
-    </script>
-@endif
